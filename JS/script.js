c348de7104d17a0bb0e33fd8f58f57f30c19272c
Emmanuel Z Weh <emmanuelzweh80@gmail.com>
2020-11-09 10:15:09 +0000

    New changes made

--- JS/script.js
+++ JS/script.js
@@ -15,25 +15,35 @@ function startGame(){
 
 }
 
+function goBack(){
+    document.getElementById("div1").style = "display: block";
+    document.getElementById("div2").style = "display: none";
+}
+
 function enter(){
     var randomNum = Math.floor(Math.random() * 100)+1;
     var inputValue = document.getElementById("guessInput").value;
+    var numberGuessed = Number(inputValue);
     var chances;
     
 
     for (chances = 10; chances >= 0; chances--){
         document.getElementById("display-outcome").style = "display: block";
-        if(inputValue == randomNum){
+        if(numberGuessed == randomNum){
             document.getElementById("guess-number-result").textContent = "YOU WIN !!!!!!";
             break;
         }
-        else if(inputValue > randomNum){
+        else if(numberGuessed > randomNum){
             document.getElementById("guess-number-result").textContent = "Guessed too high";
             document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
         } 
-        else if(inputValue < randomNum){
+        else if(numberGuessed < randomNum){
             document.getElementById("guess-number-result").textContent = "Guessed too low";
             document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
+        }
+
+        else if(chances == 0){
+            document.getElementById("guess-number-result").textContent = "YOU LOSE!!!!";
         }
         else{
             document.getElementById("guess-number-result").textContent = "Invalid input";
