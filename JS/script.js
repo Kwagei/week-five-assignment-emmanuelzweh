39f43226ee40c4020ce3384460de999b15010ed3
Emmanuel Z Weh <emmanuelzweh80@gmail.com>
2020-11-10 18:05:09 +0000

    New changes made

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
-//Random number generated
+
 var randomNum = Math.floor(Math.random() * 100)+1;
+var chances = 10;
 
 // instruction for the game
 function displayInstruction(){
@@ -26,41 +27,37 @@ function goBack(){
 function enter(){
     var inputValue = document.getElementById("guessInput").value;
     var numberGuessed = Number(inputValue);
-    var chances = 10;
-    
 
-    // for (chances = 10; chances >= 0; chances--){
-        document.getElementById("display-outcome").style = "display: block";
-        if(numberGuessed == randomNum){
-            document.getElementById("game-end-statement").textContent = "YOU WIN !!!!!!";
-            document.getElementById("end-result").style = "display: block";
-            document.getElementById("div2").style = "display: none";
+    document.getElementById("display-outcome").style = "display: block";
+    if(numberGuessed == randomNum){
+        document.getElementById("game-end-statement").textContent = "YOU WIN !!!!!!";
+        document.getElementById("end-result").style = "display: block";
+        document.getElementById("div2").style = "display: none";
             
-        }
-        else if(numberGuessed > randomNum){
-            document.getElementById("guess-number-result").textContent = "Guessed too high";
-            chances--;
-            document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
-        } 
-        else if(numberGuessed < randomNum){
-            document.getElementById("guess-number-result").textContent = "Guessed too low";
-            chances--;
-            document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
-        }
-
-        else if(chances == 0){
-            document.getElementById("game-end-statement").textContent = "YOU LOSE!!!!";
-            document.getElementById("end-result").style = "display: block";
-            document.getElementById("div2").style = "display: none";
-        }
-        else{
-            document.getElementById("guess-number-result").textContent = "Invalid input";
-            chances--;
-            document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
-        }
+    }
+    else if(numberGuessed > randomNum){
+        document.getElementById("guess-number-result").textContent = "Guessed too high";
+        chances--;
+        document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
+    } 
+    else if(numberGuessed < randomNum){
+        document.getElementById("guess-number-result").textContent = "Guessed too low";
+        chances--;
+        document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
+    }
+    else{
+        document.getElementById("guess-number-result").textContent = "Invalid input";
+        chances--;
+        document.getElementById("chances-remain").innerHTML = "Chances" + " " + chances;
+    }
+    if(chances < 0){
+        document.getElementById("game-end-statement").textContent = "YOU LOSE!!!!";
+        document.getElementById("end-result").style = "display: block";
+        document.getElementById("div2").style = "display: none";
+    }
         
 
-    }
+}
 
     function playAgain(){
         document.getElementById("div2").style = "display: block";
